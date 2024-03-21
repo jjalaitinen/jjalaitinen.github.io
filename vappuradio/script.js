d3938920a8b4d75e635479a58cf3bf3ce7a41d53
Joona Laitinen <97640519+jjalaitinen@users.noreply.github.com>
2024-03-21 08:50:57 +0200

    Vappuradiopohja ilman audio-tiedostoja

---
+++ vappuradio/script.js
@@ -0,0 +1,8 @@
+function siirra(minne) {
+    let audio = document.getElementById("eka_puolikas");
+    audio.currentTime=minne;
+}
+function siirra2(minne) {
+    let audio = document.getElementById("toka_puolikas");
+    audio.currentTime=minne;
+}
